691fbc96727b5e00801a088f4d1a79fa6afb20e0
Luis Paulo Fernandes de Barros <tc.lbarros@padtec.com.br>
2021-07-19 15:10:47 -0300

    Add Beaglebone Black kernel generation tutorial - Contains buildroot kernel, rootfs and uboot generation - Contains manual kernel, rootfs (busybox) and uboot generation

---
+++ bbblack_kernel_rootfs_uboot_gen/setup_environment.sh
@@ -0,0 +1,6 @@
+export KDIR=/opt/devel/ti-linux-kernel-dev/KERNEL
+export ARCH=arm
+export CROSS_COMPILE="ccache /opt/devel/cross_comp_arm_pru/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
+export PRU_CGT=/opt/devel/cross_comp_arm_pru/ti-cgt-pru_2.1.5
+export PRU_SSP=/opt/devel/cross_comp_arm_pru/pru-software-support-package
+export INSTALL_MOD_PATH=/opt/devel/mnt/bbb
